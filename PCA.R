cd76c7a234d4c0b3674e1e21ff57acf1fa35ed0d
Emma Dunne <39518460+emmadunne@users.noreply.github.com>
2024-06-10 18:32:39 +0200

    code for PCA plots

--- PCA.R
+++ PCA.R
@@ -8,7 +8,7 @@
 #   Supervisor: Emma Dunne (emma.dunne@fau.de)
 # ______________________________________________________
 #
-#   Raincloud plots & PCA
+#   PCA plots
 # 
 # ******************************************************
 
@@ -16,10 +16,12 @@
 ## Load package(s):
 library(tidyverse)
 library(FactoMineR) # For PCA plots
+library(ordr) # manipulating data objects
 
-library(devtools)
-install_github("fawda123/ggord") ## For PCA plots
-library(ggord)
+library(devtools) # to install ggord
+install_github("fawda123/ggord") # install ggord from GitHub
+library(ggord) # manipulating data objects
+
 
 
 
@@ -42,15 +44,15 @@ PCA_data <- subset(species_climate, select = c(occurrence_no,
 ## Load auxillery dataset to standardise the time intervals
 ints_standard <- read_csv("Data/Input/ints_standard.csv")
 
-## rename 'early interval'
-PCA_data <- rename(PCA_data, interval_std = early_interval)
+## join this aux. dataset to main PCA data
+PCA_data <- left_join(PCA_data, ints_standard, by = "early_interval")
 
-## join aux. dataset to main PCA data
-PCA_data <- left_join(PCA_data, ints_standard, by = "interval_std")
+## Remove those that have NA (stratigraphic range is too long)
+PCA_data <- na.omit(PCA_data, epoch)
 
 ## Filter to the 4 specific intervals
 ## For example, the early Cretaceous:
-PCA_data_EK <- PCA_data %>% filter(std_interval == "Early Cretaceous") 
+PCA_data_earlyK <- PCA_data %>% filter(epoch == "Early Cretaceous") 
 
 
 
@@ -60,24 +62,24 @@ PCA_data_EK <- PCA_data %>% filter(std_interval == "Early Cretaceous")
 
 
 
-eEC_pca <- PCA_data_eEC[, 2:5] %>%
+eK_pca <- PCA_data_earlyK[, 2:5] %>%
   prcomp(scale = TRUE) %>%
-  as_tbl_ord() %>% # if error, check package 'ordr' is installed correctly
-  mutate_rows(group = PCA_data_eEC$diet_group)
+  ordr::as_tbl_ord() #%>% # if error, check package 'ordr' is installed correctly
+  #mutate_rows(group = PCA_data_eEC$family)
 
-confellip_eEC <- eEC_pca %>% na.omit(diet_group) %>% 
-  ggbiplot(aes(color = group)) +
+confellip_eK <- eK_pca %>% #na.omit(diet_group) %>% 
+  ordr::ggbiplot(aes(color = family)) +
   theme_bw() +
-  geom_rows_point() +
-  geom_polygon(aes(fill = group), color = NA, alpha = .25, stat = "rows_ellipse") +
-  geom_cols_vector(color = "#444444") + # adds the arrows
-  scale_colour_manual(values = c("#0891A3", "#1E44AA", "#248528", "#FFA93D", "#572AAC")) +
-  scale_fill_manual(values = c("#0891A3", "#1E44AA", "#248528", "#FFA93D", "#572AAC"))
-confellip_eEC
+  ordr::geom_rows_point() +
+  geom_polygon(aes(fill = family), color = NA, alpha = .25, stat = "rows_ellipse") +
+  ordr::geom_cols_vector(color = "#444444") + # adds the arrows
+  scale_colour_manual(values = c("#0891A3", "#1E44AA", "#248528", "#FFA93D", "#572AAC")) + ## add more colours if >5 families!
+  scale_fill_manual(values = c("#0891A3", "#1E44AA", "#248528", "#FFA93D", "#572AAC")) ## add more colours if >5 families!
+confellip_eK
 
 
 
-
+## repeat for each interval!
 
 
 
